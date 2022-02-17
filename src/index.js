6d4d5dae60a596664b52b6a3d12b7fc2a67c1cf4
vikrantkale1201 <vikrantkale1201@gmail.com>
2022-02-17 21:35:20 +0530

    Facebook Clone React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*
+// component, JSX
+// Using Normal Function
+function Hello(){
+    return(
+        <h1> Hello World </h1>
+    )
+}
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// component, JSX
+// Using Arrow Function (to make arrow function write function name = () => { })
+const Hello=()=>{
+    return(
+        // Use <div> </div> while returning multiple tags
+        <div>
+        <h1 style={
+                {color : "red", backgroundColor : "black",
+                margin : "10px", padding : "10px", width : "170px"}
+            }> Hello World </h1>
+
+        <button className="button"> Apple </button>
+        <button className="button"> Mango </button>
+        <button className="button"> Banana </button>
+        </div>
+    )
+}
+
+ReactDOM.render(<Hello/>, document.getElementById("root"));
+// ReactDOM used for rendering (rendering reqd to show JSX in browser)
+// For rendering the component, Use Component name in <Hello></Hello>
+// OR
+// Use Component name in self ending <Hello/>
+// So it will print output
+// Here we get element of id "root" from index.html file
+// And rendered above JSX code in that Div (id= root)
